8c8fd9082919c1594682ffdc58a30378d0ec90e0
Rizki Akbar <44969449+akasa885@users.noreply.github.com>
2022-05-20 15:23:03 +0700

    add_try_catch_for_file_uploading

--- app/Http/Traits/FileUploadTrait.php
+++ app/Http/Traits/FileUploadTrait.php
@@ -5,11 +5,16 @@ trait FileUploadTrait {
 
     public function saveInvoice($file)
     {
-        $fileNameWithExt = $file->getClientOriginalName();
-        $filename = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
-        $extension = $file->getClientOriginalExtension();
-        $filenameSimpan = $filename.'__'.time().'.'.$extension;
-        $path = $file->storeAs('public/bukti_image', $filenameSimpan);
-        return $filenameSimpan;
+        try {
+            $fileNameWithExt = $file->getClientOriginalName();
+            $filename = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+            $extension = $file->getClientOriginalExtension();
+            $filenameSimpan = $filename.'__'.time().'.'.$extension;
+            $path = $file->storeAs('public/bukti_image', $filenameSimpan);
+            return $filenameSimpan;
+        } catch (\Throwable $th) {
+            //throw $th;
+            return false;
+        }
     }
 }
