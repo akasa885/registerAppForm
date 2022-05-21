c416b052318e4e01a6f2730d8642e3e9cc066905
Rizki Akbar <44969449+akasa885@users.noreply.github.com>
2022-05-21 13:01:51 +0700

    change_case_view

--- resources/views/pages/pendaftaran/upPay.blade.php
+++ resources/views/pages/pendaftaran/upPay.blade.php
@@ -7,8 +7,6 @@
 
             @if($expire)
                 <h3 class="font-semibold p-10 sm:mx-auto">Link Pembayaran Telah Kadaluarsa</h3>
-            @elseif($used)
-            <h3 class="font-semibold p-10 sm:mx-auto">Link Pembayaran Telah Digunakan</h3>
             @elseif($message = Session::get('success'))
             <div class="alert bg-green-100 rounded-lg py-5 px-6 text-base text-green-700 inline-flex items-center w-full alert-dismissible fade show" role="alert">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
@@ -17,6 +15,8 @@
                 {{ $message }}
                 <button type="button" class="btn-close box-content w-4 h-4 p-1 ml-auto text-yellow-900 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-yellow-900 hover:opacity-75 hover:no-underline" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
+            @elseif($used)
+            <h3 class="font-semibold p-10 sm:mx-auto">Link Pembayaran Telah Digunakan</h3>
             @else
                 <header class="font-semibold flex justify-center bg-gray-200 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
                     Form Bukti Bayar {{$link->title}} <br/>
