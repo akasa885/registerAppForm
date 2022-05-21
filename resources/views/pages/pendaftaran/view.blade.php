3e74168452b78f608896fd094c6b8ba341a27cd6
Rizki Akbar <44969449+akasa885@users.noreply.github.com>
2022-05-21 13:00:51 +0700

    add_option_case_for_free_event

--- resources/views/pages/pendaftaran/view.blade.php
+++ resources/views/pages/pendaftaran/view.blade.php
@@ -5,7 +5,20 @@
     <div class="lg:w-6/12 sm:px-6 sm:w-10/12">
         <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
 
-            @if($show)
+            @php
+             $count = App\Models\Link::find($link->id);
+             $count->viewed_count = $count->viewed_count + 1;
+             $count->save();
+            @endphp
+            @if($message = Session::get('success'))
+            <div class="alert bg-green-100 rounded-lg py-5 px-6 text-base text-green-700 inline-flex items-center w-full alert-dismissible fade show" role="alert">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path>
+                </svg>
+                {{ $message }}
+                <button type="button" class="btn-close box-content w-4 h-4 p-1 ml-auto text-yellow-900 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-yellow-900 hover:opacity-75 hover:no-underline" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            @elseif($show)
                 <h3 class="font-semibold p-10 sm:mx-auto">Link Pendaftaran Tidak Tersedia</h3>
             @else
                 <header class="font-semibold flex justify-center bg-gray-200 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
