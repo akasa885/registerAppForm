9c5e2cb0e756c4987f6a43330dcb7f31a59ddbfe
Rizki Akbar <44969449+akasa885@users.noreply.github.com>
2022-05-21 13:14:44 +0700

    send_mail_confirmed_pay

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -1,9 +1,19 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+//mail
+use App\Mail\ConfirmedPay;
 
 use App\Models\Member;
-use Illuminate\Http\Request;
+use App\Models\Invoice;
+use App\Models\Email;
+
+use Carbon\Carbon;
 
 class MemberController extends Controller
 {
@@ -81,5 +91,60 @@ class MemberController extends Controller
     public function destroy(Member $member)
     {
         //
+    }
+
+    public function viewSheet($id)
+    {
+        $data = Member::findorfail($id);
+        if ($data->bukti_bayar == null) {
+            return response()->json(['success' => false, 'message' => 'Bukti tidak ada!'], 404);
+        }
+        return response()->json(['success' => true, 'message' => 'Bukti ditemukan', 'bukti' => asset('storage/bukti_image').'/'.$data->bukti_bayar, 'memberId' => $data->id], 200);
+    }
+
+    public function updateBukti(Request $request, Member $member)
+    {
+        try {
+            $member = $member->findorfail($request->id);
+            $invoice = Invoice::findorfail($member->id);
+            $invoice->status = 2;
+
+            $this->sendMailPaymentReceived($member->link, $member);
+            $invoice->save();
+
+            return response()->json(['success' => true, 'message' => 'Pembayaran Diterima']);
+        } catch (\Throwable $th) {
+            // throw $th;
+            // return $member->link;
+            return response()->json(['success' => false, 'message' => 'Request Error'], 500);
+        }
+    }
+
+    public function sendMailPaymentReceived($link, $member){
+        foreach($link->mails as $item){
+            if($item->type == 'confirmed'){
+                $information = $item->information;
+            }
+        }
+        $data = array(
+            'name'      =>  $member->full_name,
+            'acara'     => $link->title,
+            'message'   =>   $information,
+        );
+        $from_mail = Email::EMAIL_FROM;
+
+        try {
+            Mail::to($member->email)->send(new ConfirmedPay($data, $from_mail));
+            $mail_db = new Email;
+            $mail_db->send_from = $from_mail;
+            $mail_db->send_to = $member->email;
+            $mail_db->message = $information;
+            $mail_db->user_id = $member->id;
+            $mail_db->type_email = Email::TYPE_EMAIL[2];
+            $mail_db->sent_count = 1;
+            $mail_db->save();
+        } catch (\Throwable $th) {
+            abort(500);
+        }
     }
 }
