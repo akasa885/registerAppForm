de6160652357741ac4fe197a88059ee168a9ca9a
Rizki Akbar <akasa2444@gmail.com>
2023-02-27 14:55:29 +0700

    create memberProfile model and migration

---
+++ database/migrations/2023_02_27_144812_create_member_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemberProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('password');
+            $table->string('phone_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member_profiles');
+    }
+}
