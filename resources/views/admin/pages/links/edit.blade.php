0f999b9359453ff87b8faf29896f5b1775a5724e
Rizki Akbar <akasa2444@gmail.com>
2023-03-02 00:20:26 +0700

    create edit blade based on admin/links

---
+++ resources/views/admin/pages/links/edit.blade.php
@@ -0,0 +1,41 @@
+@extends('admin.layouts.app')
+@section('title', 'Mengedit Link')
+@section('content')
+<div class="container">
+    <form action="{{ route('admin.link.update', ['id' => $link_detail->id]) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <input type="hidden" name="event_type" value="{{$type_reg}}">
+        @if ($type_reg = 'pay')
+        @include('admin.pages.links.edit_pay')
+        @elseif ($type_reg = 'free')
+        @include('admin.pages.links.edit_free')
+        @else
+        <h2> Requested Url Not Found ! </h2>
+        @endif
+    </form>
+</div>     
+@endsection
+
+@push('scripts')
+<script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
+<script src="//cdn.ckeditor.com/4.17.2/standard/ckeditor.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#datepicker-1').datepicker({
+            format: 'dd-mm-yyyy'
+        });
+        $('#datepicker-2').datepicker({
+            format: 'dd-mm-yyyy'
+        });
+    });
+    var options = {
+        filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+        filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+        filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+        filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+    };
+
+    CKEDITOR.replace('my-editor-1', options);
+</script>
+@endpush
--- resources/views/admin/pages/links/edit_free.blade.php
+++ resources/views/admin/pages/links/edit_free.blade.php
@@ -1,126 +1,104 @@
-@extends('admin.layouts.app')
-@section('title', 'Mengedit Link')
-@section('content')
-<div class="container">
-    <form action="{{ route('admin.link.update', ['id' => $link_detail->id]) }}" method="POST">
-        @csrf
-        <input type="hidden" name="event_type" value="pay">
-    <div class="row justify-content-center">
-        <div class="col-md-12 col-lg-12">
-            <div class="mb-3 card">
-                <div class="card-header-tab card-header-tab-animation card-header">
-                    <div class="card-header-title">
-                        <i class="header-icon lnr-apartment icon-gradient bg-love-kiss"> </i>
-                        Form Pendaftaran Customisasi
-                    </div>
-                    <div class="btn-actions-pane-right">
-                        <div class="nav">
-                            <button type="submit" name="status" value="1" class="border-0 btn-transition btn btn-outline-success">Save</button>
-                            <a href="{{ route('admin.link.view') }}" class="border-0 btn-transition  btn btn-outline-danger">Cancel</a>
-                        </div>
+<!-- Start Content Edit -->
+<div class="row justify-content-center">
+    <div class="col-md-12 col-lg-12">
+        <div class="mb-3 card">
+            <div class="card-header-tab card-header-tab-animation card-header">
+                <div class="card-header-title">
+                    <i class="header-icon lnr-apartment icon-gradient bg-love-kiss"> </i>
+                    Form Pendaftaran Customisasi
+                </div>
+                <div class="btn-actions-pane-right">
+                    <div class="nav">
+                        <button type="submit" name="status" value="1"
+                            class="border-0 btn-transition btn btn-outline-success">Save</button>
+                        <a href="{{ route('admin.link.view') }}"
+                            class="border-0 btn-transition  btn btn-outline-danger">Cancel</a>
                     </div>
                 </div>
-            </div>    
-        </div>                
-        <div class="col-md-8">            
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <div class="mb-3">
-                            <label for="title" class="form-label">Judul</label>
-                            <input type="text" required class="form-control" value="{{$link_detail->title}}" name="title" id="title">
-                        </div>
-                        <div class="mb-3">
-                            <label for="desc" class="form-label">Deskripsi</label>
-                            <textarea required name="desc" placeholder="deskripsi acara" class="my-editor form-control" id="my-editor-1" cols="30" rows="10">{!!$link_detail->description!!}</textarea>
-                        </div>
-                    </div>                    
-                </div>                              
+            </div>
         </div>
-        <div class="col-md-4"> 
-            <div class="card mb-3 d-flex flex-column feature-img">
-                <div class="card-header-tab card-header-tab-animation card-header">
-                    <div class="card-header-title">                        
-                        Banner
-                    </div>                    
-                </div> 
-                <div class="col-auto align-self-center my-2">
-                    <div class="fileinput-new thumbnail" id="holder" style="max-width: 200px; max-height: 150px;">
-                        <img id="previewimg_thumb" src="
-                        @php 
-                        $temp = explode('/', $link_detail->banner);
-                        $size = sizeof($temp);
-                        if($link_detail->banner == null){
-                            echo asset('/images/default/no-image.png');
-                        }else{
-                            $url2= str_replace(basename($link_detail->banner) , '', $link_detail->banner  ) ;
-                            $url2=$url2.'thumbs/'.basename($link_detail->banner);
-                        
-                            echo $url2;
-                        }
-                        @endphp" alt="banner" />
-                    </div>
-                  </div>
-                  <div class="col p-4">
-                    <div class="input-group">
-                        <span class="input-group-btn">
-                          <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
-                            <i class="fa fa-picture-o"></i> Choose
-                          </a>
-                        </span>
-                        <input id="thumbnail" class="form-control" value="{{$link_detail->banner}}" type="text" name="filepath">
-                      </div>
-                </div> 
-            </div>
-            <div class="card mb-3 d-flex flex-column">
-                <div class="card-header-tab card-header-tab-animation card-header">
-                    <div class="card-header-title">
-                        Tanggal Pendaftaran Dibuka
-                    </div>
-                </div>
-                <div class="col-auto align-self-center my-2">
-                    <div class="form-group">
-                        <input type='text' name="open_date" value="{{ date('d-m-Y', strtotime($link_detail->active_from)) }}" required class="form-control" id='datepicker-1' placeholder="dd-mm-yy">
-                    </div>
-                </div>
-            </div>
-            <div class="card mb-3 d-flex flex-column">
-                <div class="card-header-tab card-header-tab-animation card-header">
-                    <div class="card-header-title">
-                        Tanggal Pendaftaran Ditutup
-                    </div>
-                </div>
-                <div class="col-auto align-self-center my-2">
-                    <div class="form-group">
-                        <input type='text' name="close_date" 
-                        value="{{ date('d-m-Y', strtotime($link_detail->active_until)) }}" required class="form-control" id='datepicker-2' placeholder="dd-mm-yy">
-                    </div>
-                </div>
-            </div>
-        </div>        
     </div>
-    </form>
-</div>     
-@endsection
-
-@push('scripts')
-<script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
-<script src="//cdn.ckeditor.com/4.17.2/standard/ckeditor.js"></script>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('#datepicker-1').datepicker({
-            format: 'dd-mm-yyyy'
-        });
-        $('#datepicker-2').datepicker({
-            format: 'dd-mm-yyyy'
-        });
-    });
-    var options = {
-        filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
-        filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
-        filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
-        filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
-    };
-
-    CKEDITOR.replace('my-editor-1', options);
-</script>
-@endpush
+    <div class="col-md-8">
+        <div class="card mb-3">
+            <div class="card-body">
+                <div class="mb-3">
+                    <label for="title" class="form-label">Judul</label>
+                    <input type="text" required class="form-control" value="{{ $link_detail->title }}"
+                        name="title" id="title">
+                </div>
+                <div class="mb-3">
+                    <label for="desc" class="form-label">Deskripsi</label>
+                    <textarea required name="desc" placeholder="deskripsi acara" class="my-editor form-control" id="my-editor-1"
+                        cols="30" rows="10">{!! $link_detail->description !!}</textarea>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card mb-3 d-flex flex-column feature-img">
+            <div class="card-header-tab card-header-tab-animation card-header">
+                <div class="card-header-title">
+                    Banner
+                </div>
+            </div>
+            <div class="col-auto align-self-center my-2">
+                <div class="fileinput-new thumbnail" id="holder"
+                    style="max-width: 200px; max-height: 150px;">
+                    <img id="previewimg_thumb"
+                        src="
+            @php $temp = explode('/', $link_detail->banner);
+            $size = sizeof($temp);
+            if($link_detail->banner == null){
+                echo asset('/images/default/no-image.png');
+            }else{
+                $url2= str_replace(basename($link_detail->banner) , '', $link_detail->banner  ) ;
+                $url2=$url2.'thumbs/'.basename($link_detail->banner);
+            
+                echo $url2;
+            } @endphp"
+                        alt="banner" />
+                </div>
+            </div>
+            <div class="col p-4">
+                <div class="input-group">
+                    <span class="input-group-btn">
+                        <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
+                            <i class="fa fa-picture-o"></i> Choose
+                        </a>
+                    </span>
+                    <input id="thumbnail" class="form-control" value="{{ $link_detail->banner }}" type="text"
+                        name="filepath">
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 d-flex flex-column">
+            <div class="card-header-tab card-header-tab-animation card-header">
+                <div class="card-header-title">
+                    Tanggal Pendaftaran Dibuka
+                </div>
+            </div>
+            <div class="col-auto align-self-center my-2">
+                <div class="form-group">
+                    <input type='text' name="open_date"
+                        value="{{ date('d-m-Y', strtotime($link_detail->active_from)) }}" required
+                        class="form-control" id='datepicker-1' placeholder="dd-mm-yy">
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 d-flex flex-column">
+            <div class="card-header-tab card-header-tab-animation card-header">
+                <div class="card-header-title">
+                    Tanggal Pendaftaran Ditutup
+                </div>
+            </div>
+            <div class="col-auto align-self-center my-2">
+                <div class="form-group">
+                    <input type='text' name="close_date"
+                        value="{{ date('d-m-Y', strtotime($link_detail->active_until)) }}" required
+                        class="form-control" id='datepicker-2' placeholder="dd-mm-yy">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Content Edit -->
