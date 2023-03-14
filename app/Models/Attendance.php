a69f48e3332f891286621032e8ab93dfde208aa4
Rizki Akbar <akasa2444@gmail.com>
2023-03-15 00:32:20 +0700

    add relation at Attendance model

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Attendance extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'attendance_path',
+        'link_id',
+        'active_from',
+        'active_until',
+        'created_by',
+    ];
+
+    public function scopeOwnAttendance($query)
+    {
+        return $query->where('created_by', auth()->user()->id);
+    }
+
+    public function link()
+    {
+        return $this->belongsTo(Link::class, 'link_id', 'id');
+    }
+
+    public function member_attend()
+    {
+        return $this->hasMany(MemberAttend::class, 'attend_id', 'id');
+    }
 }
