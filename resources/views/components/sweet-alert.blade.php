a8c360760ba993c826038ebe641c26e2a3aa1042
Rizki Akbar <akasa2444@gmail.com>
2023-03-14 23:57:56 +0700

    sweetAlert component

---
+++ resources/views/components/sweet-alert.blade.php
@@ -0,0 +1,7 @@
+@push('up_styles')
+<link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.min.css" rel="stylesheet">
+@endpush
+
+@push('up_scripts')
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.all.min.js"></script>
+@endpush
