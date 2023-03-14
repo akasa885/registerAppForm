ffd1b9cfd7fe74fb6f2d2f284e66fabdd25c9f03
Rizki Akbar <akasa2444@gmail.com>
2023-03-15 00:20:58 +0700

    fill memberAttend component

--- app/Models/MemberAttend.php
+++ app/Models/MemberAttend.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class MemberAttend extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'attend_id',
+        'member_id',
+    ];
+
+    public function attendance()
+    {
+        return $this->belongsTo(Attendance::class, 'attend_id', 'id');
+    }
+
+    public function member()
+    {
+        return $this->belongsTo(Member::class, 'member_id', 'id');
+    }
 }
