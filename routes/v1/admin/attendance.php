b1b02fd011db01b1229b510e8b61bab0b4d3f046
Rizki Akbar <akasa2444@gmail.com>
2023-03-15 00:32:36 +0700

    add route dt at attendance admin route

--- routes/v1/admin/attendance.php
+++ routes/v1/admin/attendance.php
@@ -6,4 +6,5 @@ Route::get('/', [Attendancecontroller::class, 'index'])->name('view');
 Route::get('/create/session/{type}', [Attendancecontroller::class, 'create'])->where(['type' => 'day|hourly'])->name('create');
 Route::post('/store/session/{type}', [Attendancecontroller::class, 'store'])->where(['type' => 'day|hourly'])->name('store');
 Route::delete('/delete/session/{attendance}', [Attendancecontroller::class, 'destroy'])->name('delete');
+Route::get('/dt/attendance', [Attendancecontroller::class, 'dtb_attendance'])->name('dt.attendance');
 Route::get('/dtable', [Attendancecontroller::class, 'dtb_member'])->name('dtable');
