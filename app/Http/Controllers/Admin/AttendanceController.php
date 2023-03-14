94f47f01092b5a56f90a472bfaae19a8d9bcea82
Rizki Akbar <akasa2444@gmail.com>
2023-03-14 23:24:45 +0700

    add process on create & store at AttendanceController

--- app/Http/Controllers/Admin/AttendanceController.php
+++ app/Http/Controllers/Admin/AttendanceController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use App\Models\Attendance;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Models\Attendance;
+use App\Models\Link;
+
+use App\Http\Traits\GenerateTokenUniqueColumnTrait;
+
+use App\Http\Requests\AttendanceRequest;
 
 class AttendanceController extends Controller
 {
+    use GenerateTokenUniqueColumnTrait;
     /**
      * Display a listing of the resource.
      *
@@ -25,7 +32,10 @@ class AttendanceController extends Controller
      */
     public function create($type)
     {
-        //
+        $links_active = Link::filterActiveMyLinks();
+        $links = auth()->user()->links;
+        
+        return view('admin.pages.attendance.create', compact('type', 'links'));
     }
 
     /**
@@ -34,9 +44,27 @@ class AttendanceController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AttendanceRequest $request, $type)
     {
-        //
+        try {
+            $validated = $request->validated();
+            $attend = Attendance::select('attendance_path')->get();
+            $token = $this->getToken($attend->toArray(), 'attendance_path', 6);
+            $validated['attendance_path'] = $token;
+
+            // create attendance
+            $attendance = Attendance::create($validated);
+
+            return redirect()->route('admin.attendance.view')->with('success', 'Attendance created successfully');
+
+
+        } catch (\Throwable $th) {
+            if (config('app.debug'))
+                throw $th;
+            
+            return back()->with('error', 'Something went wrong, failed create attendance');
+        }
+        
     }
 
     /**
