9a16cb40183762f848d7c50dc74bce960aaad56b
Rizki Akbar <akasa2444@gmail.com>
2023-03-15 00:43:57 +0700

    add route get detail members at attendance admin route

--- routes/v1/admin/attendance.php
+++ routes/v1/admin/attendance.php
@@ -6,5 +6,6 @@ Route::get('/', [Attendancecontroller::class, 'index'])->name('view');
 Route::get('/create/session/{type}', [Attendancecontroller::class, 'create'])->where(['type' => 'day|hourly'])->name('create');
 Route::post('/store/session/{type}', [Attendancecontroller::class, 'store'])->where(['type' => 'day|hourly'])->name('store');
 Route::delete('/delete/session/{attendance}', [Attendancecontroller::class, 'destroy'])->name('delete');
+Route::get('/attending/members/{attendance}', [Attendancecontroller::class, 'show'])->name('detail');
 Route::get('/dt/attendance', [Attendancecontroller::class, 'dtb_attendance'])->name('dt.attendance');
 Route::get('/dtable', [Attendancecontroller::class, 'dtb_member'])->name('dtable');
