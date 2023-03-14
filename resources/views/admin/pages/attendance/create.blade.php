9b40187e935cc6b4551f173ec284897c14a6fb22
Rizki Akbar <akasa2444@gmail.com>
2023-03-14 15:41:32 +0700

    create create blade attendance session

---
+++ resources/views/admin/pages/attendance/create.blade.php
@@ -0,0 +1,28 @@
+@extends('admin.layouts.app')
+@section('title', 'Mengedit Link')
+@section('content')
+<div class="container">
+    <form action="{{ route('admin.attendance.store', ['type' => $type]) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <input type="hidden" name="attendance_type" value="{{$type}}">
+        @if ($type == 'day')
+        @include('admin.pages.attendance.cDay')
+        @elseif ($type == 'hourly')
+        @include('admin.pages.attendance.cHourly')
+        @else
+        <h2> Requested Url Not Found ! </h2>
+        @endif
+    </form>
+</div>     
+@endsection
+
+@push('scripts')
+<script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
+<script src="//cdn.ckeditor.com/4.17.2/standard/ckeditor.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+
+    });
+</script>
+@endpush
