926a7460e7c6841c177d596bc1570b94c591cc2c
Rizki Akbar <akasa2444@gmail.com>
2023-03-14 23:22:43 +0700

    create attedanceRequest component

---
+++ app/Http/Requests/AttendanceRequest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Link;
+
+class AttendanceRequest extends FormRequest
+{
+    private $ver_control = 'v0';
+    public $type_control;
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return $this->customRoles($this->attendance_type ?? 'day');
+    }
+
+    public function customRoles($type)
+    {
+        $this->type_control = $type;
+
+        $roles = [];
+        $roles = [
+            'attendance_type' => ['required', 'string', 'in:day,hourly'],
+            'selected_event' => ['required', 'string', 'exists:links,link_path'],
+        ];
+
+        switch ($type){
+            case 'day' : 
+                $roles = array_merge($roles, [
+                    'date' => ['required', 'date'],
+                ]);
+                break;
+            case 'hourly' :
+                $roles = array_merge($roles, [
+                    'datetime_start' => ['required', 'date'],
+                    'datetime_end' => ['required', 'date', 'after:datetime_start'],
+                ]);
+                break;
+        }
+
+        return $roles;
+    }
+
+    public function attributes()
+    {
+        $attributes = [];
+        $attributes = [
+            'selected_event' => 'Event',
+        ];
+
+        switch($this->type_control){
+            case 'day' :
+                $attributes = array_merge($attributes, [
+                    'date' => 'Hari',
+                ]);
+                break;
+            case 'hourly' :
+                $attributes = array_merge($attributes, [
+                    'datetime_start' => 'Waktu Buka',
+                    'datetime_end' => 'Waktu Tutup',
+                ]);
+                break;
+        }
+
+        return $attributes;
+    }
+
+    public function validated()
+    {
+        $validated = parent::validated();
+        if ($this->type == 'day')
+        {
+            $validated['active_from'] = date('Y-m-d H:i:s', strtotime($validated['date'] . ' 00:00:00'));
+            $validated['active_until'] = date('Y-m-d H:i:s', strtotime($validated['date'] . ' 23:59:59'));
+            unset($validated['date']);
+        } else if ($this->type == 'hourly')
+        {
+            $validated['active_from'] = date('Y-m-d H:i:s', strtotime($validated['datetime_start']));
+            $validated['active_until'] = date('Y-m-d H:i:s', strtotime($validated['datetime_end']));
+            unset($validated['date']);
+            unset($validated['datetime_start']);
+            unset($validated['datetime_end']);
+        }
+        $validated['link_id'] = Link::where('link_path', $validated['selected_event'])->first()->id;
+        $validated['created_by'] = auth()->user()->id;
+        unset($validated['selected_event']);
+
+        return $validated;
+    }
+}
