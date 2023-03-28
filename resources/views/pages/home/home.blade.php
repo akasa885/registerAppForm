a642ced30e79122f48753fea7d7757a1ac20b61c
Rizki Akbar <akasa2444@gmail.com>
2023-03-28 14:38:31 +0700

    change home card event view at home blade

--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -6,6 +6,17 @@
     <div class="p-10 grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-5">
         <!--Card-->
         @foreach ($link as $item)
+        @php
+            $open = false;
+            $close = false;
+            if ($date >= date('Y-m-d', strtotime($item->active_from)) && $date <= date('Y-m-d', strtotime($item->active_until))) {
+                $open = true;
+            } elseif($date < date('Y-m-d', strtotime($item->active_from))) {
+                $open = false;
+            } else {
+                $close = true;
+            }
+        @endphp
             <a href="{{ route('form.link.view', ['link' => $item->link_path]) }}">
                 <div class="rounded overflow-hidden shadow-lg">
                     <img class="w-full transition bg-cool-gray-200 duration-300 object-contain h-72 ease-in-out md:h-64 sm:h-56 hover:scale-110"
@@ -20,19 +31,26 @@
                     </div>
                     <div class="px-6 pt-4 pb-2">
                         <div class="grid grid-rows-2 grid-flow-col gap-1">
+                            @if(!$open && !$close)
                             <span
                                 class="row-span-1 w-50 inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">Open
                                 Registration : {{ date('d/m/y', strtotime($item->active_from)) }}</span>
+                            @elseif($open && !$close)
                             <span
                                 class="row-span-1 w-50 inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">Close
                                 Registration : {{ date('d/m/y', strtotime($item->active_until)) }}</span>
+                            @else
+                            <span
+                                class="row-span-1 w-50 inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">Close
+                                Registration : {{ date('d/m/y', strtotime($item->active_until)) }}</span>
+                            @endif
                         </div>
                         <div class="flex justify-between">
                             <div class="col-auto">
-                                @if ($date >= date('Y-m-d', strtotime($item->active_from)) && $date <= date('Y-m-d', strtotime($item->active_until)))
+                                @if ($open && !$close)
                                     <span
                                         class="inline-block bg-green-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#Open</span>
-                                @elseif($date < date('Y-m-d', strtotime($item->active_from)))
+                                @elseif(!$open && !$close)
                                     <span
                                         class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#Not
                                         Open Yet</span>
