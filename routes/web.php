91a552b9b697bb653867a17b39896f48519649ba
Rizki Akbar <akasa2444@gmail.com>
2023-03-29 10:45:05 +0700

    add export route into admin side web route

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,10 @@ Route::prefix('dpanel')->name('admin.')->group(function(){
         Route::prefix('users-setting')->name('users.')->group(__DIR__.'/v1/admin/setting_user.php');
         // < ------------------------------- Users Admin ----------------------------------------- >
 
+        // < -------------------------------Export ----------------------------------------- >
+        Route::prefix('export')->name('export.')->group(__DIR__.'/v1/export.php');
+        // < -------------------------------Export ----------------------------------------- >
+
         // < ------------------------------- Members Admin ----------------------------------------- >
         Route::prefix('member')->name('member.')->group(function(){
             Route::get('/pay-sheet/{id}', [MemberController::class, 'viewSheet'])->name('lihat.bukti');
