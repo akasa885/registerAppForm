18c57450a7554d3b1f9592db26280ce28775957a
Rizki Akbar <akasa2444@gmail.com>
2023-03-29 11:21:28 +0700

    fix type at export route

--- routes/v1/export.php
+++ routes/v1/export.php
@@ -1,5 +1,5 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\ExportController;
+use App\Http\Controllers\ExportController;
 
 Route::get('/member-export/{link}', [ExportController::class, 'memberExport'])->name('member-export');
