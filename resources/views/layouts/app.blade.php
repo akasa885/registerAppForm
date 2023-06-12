46e0a6b35e5e2cedfdef02c182bcacce435123e8
Rizki Akbar <akasa2444@gmail.com>
2023-06-12 16:05:18 +0700

    add icon into web front app layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
 
     <title>@yield('title', config('app.name', 'Laravel'))</title>
 
+    <link rel="shortcut icon" href="{{ asset('icon/favicon.png') }}" />
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     
