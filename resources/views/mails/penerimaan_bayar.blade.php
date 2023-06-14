05bc45292b1396499226e2100f48405fc950dcc9
Rizki Akbar <akasa2444@gmail.com>
2023-06-15 01:00:28 +0700

    change mail penerimaan_bayar

--- resources/views/mails/penerimaan_bayar.blade.php
+++ resources/views/mails/penerimaan_bayar.blade.php
@@ -1,15 +1,56 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-</head>
-<body>
-    <p>Kepada : <strong>{{ucwords($data['name'])}}</strong></p>
-    <p>Yang mendaftar pada acara <strong>{{$data['acara']}}</strong></p>
-    {!!$data['message']!!}
-    <p>---------- Terima kasih telah menggunakan layanan kami. -----------</p>
-    <p><a href="https://upquality.net">Website Kami.</a></p>
-</body>
-</html>
+@extends('layouts.mail')
+@section('mail_content')
+    <table class="wrapper" width="100%" cellpadding="0" cellspacing="0" style="border:none;">
+        <tr>
+            <td width="100%" style="background-color: #f25454; color: #FFFFFF;">
+                <p class="heading"
+                    style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; line-height: 1.5em; text-align: center;">
+                    Bukti Bayar Diterima
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td class="body" width="100%" cellpadding="0" cellspacing="0"
+                style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; margin: 0; width: 100%; ">
+                <table class="wrapper" cellpadding="0" cellspacing="0" width="100%"
+                    style="border: none; background-color: #ffffff; border-bottom: 1px solid #e8e5ef; border-top: 1px solid #e8e5ef; padding: 1.5em;">
+                    <tr>
+                        <td style="border:none;">
+                            <p>Kepada : <strong>{{ __(ucwords($data['name'])) }}</strong> </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border:none;">
+                            <p>Yang mendaftar pada acara <strong>{{ $data['acara'] }}</strong></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border:none;">
+                            <p>{!! $data['message'] !!}</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border:none;">
+                            <table class="wrapper" width="100%" style="border:none;">
+                                <tr>
+                                    <td>
+                                        <p><strong>Terima Kasih Telah Menggunakan Layanan Kami !</strong></p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <table width="100%">
+                <tr>
+                    <td>
+                        <p>@include('includes.mail_footer')</p>
+                    </td>
+                </tr>
+            </table>
+        </tr>
+    </table>
+@endsection
