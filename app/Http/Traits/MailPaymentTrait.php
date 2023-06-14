7e56d3057c81dc1112f5c321841b6c05c11817fe
Rizki Akbar <akasa2444@gmail.com>
2023-06-15 02:11:12 +0700

    add sendMailPaymentReceived at MailPaymentTrait

--- app/Http/Traits/MailPaymentTrait.php
+++ app/Http/Traits/MailPaymentTrait.php
@@ -8,6 +8,8 @@ use App\Models\Email;
 
 use App\Mail\ConfirmPay;
 use App\Mail\RejectedPay;
+use App\Mail\EventInfo;
+use App\Mail\ConfirmedPay;
 
 trait MailPaymentTrait {
 
@@ -25,6 +27,36 @@ trait MailPaymentTrait {
         $mail_tmp = MailPayment::where('link_id', $link->id)->where('type', $type)->first();
         $mail_tmp->information = $email;
         $mail_tmp->save();
+    }
+
+    public function sendMailPaymentReceived($link, $member){
+        foreach($link->mails as $item){
+            if($item->type == 'confirmed'){
+                $information = $item->information;
+            }
+        }
+        $data = array(
+            'name'      =>  $member->full_name,
+            'acara'     => $link->title,
+            'message'   =>   $information,
+        );
+        $from_mail = Email::EMAIL_FROM;
+
+        try {
+            Mail::to($member->email)->send(new ConfirmedPay($data, $from_mail));
+            $mail_db = new Email;
+            $mail_db->send_from = $from_mail;
+            $mail_db->send_to = $member->email;
+            $mail_db->message = $information;
+            $mail_db->user_id = $member->id;
+            $mail_db->type_email = Email::TYPE_EMAIL[2];
+            $mail_db->sent_count = 1;
+            $mail_db->save();
+        } catch (\Throwable $th) {
+            Log::error('Error send mail payment received');
+            Log::error($th->getMessage());
+            throw $th;
+        }
     }
 
     public function sendMailPayment($link, $member, $invoice, $reject = false, $message = null){
