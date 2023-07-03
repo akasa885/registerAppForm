a1650aeaa1480091f58a63f7f147153fb300ad90
Rizki Akbar <akasa2444@gmail.com>
2023-07-03 09:46:16 +0700

    change html blade expired_link

--- resources/views/pages/pendaftaran/pay_confirmation/expired_link.blade.php
+++ resources/views/pages/pendaftaran/pay_confirmation/expired_link.blade.php
@@ -1,9 +1,16 @@
 @if (config('app.locale') == 'id')
-    <h3 class="font-semibold p-10 sm:mx-auto">Link Konfirmasi Pembayaran Telah Kadaluarsa</h3>
+    <h3 class="font-semibold px-10 py-5 sm:mx-auto">Link Konfirmasi Pembayaran Telah Kadaluarsa</h3>
 @else
-    <h3 class="font-semibold p-10 sm:mx-auto">Payment Confirmation Link Has Expired</h3>
+    <h3 class="font-semibold px-10 py-5 sm:mx-auto">Payment Confirmation Link Has Expired</h3>
 @endif
-<form action="{{ route('form.pay.renew') }}" method="POST">
-    @csrf
-    <input type="hidden" name="payment_code" value="{{ $pay_code }}">
-</form>
+    <p class="px-10 py-5 pb-10 sm:mx-auto text-center">{{ $message }}</p>
+    <div class="flex justify-center py-5 align-center">
+        <a href="{{ $route_form }}"
+            class="bg-transparent flex-fill hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            @if (config('app.console') == 'id')
+                Kembali Ke Form
+            @else
+                Back To Form
+            @endif
+        </a>
+    </div>
