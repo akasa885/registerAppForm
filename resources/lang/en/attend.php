7cdedd9b9a217c957b6a6615af9ed966737198d3
Rizki Akbar <akasa2444@gmail.com>
2023-07-07 16:12:26 +0700

    create lang pack attend

---
+++ resources/lang/en/attend.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'form' => [
+        'email' => 'Email Address',
+        'phone_number' => 'Phone Number',
+        'email_helper' => 'Email used to register',
+        'phone_helper' => 'Phone number used to register',
+    ]
+];
---
+++ resources/lang/id/attend.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'form' => [
+        'email' => 'Alamat Email',
+        'phone_number' => 'Nomor Telepon',
+        'email_helper' => 'Email yang digunakan untuk mendaftar',
+        'phone_helper' => 'Nomor telepon yang digunakan untuk mendaftar',
+    ]
+];
