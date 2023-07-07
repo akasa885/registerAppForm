1cfe01a2f1f5da744abbf96a8c35febc66c6e92c
Rizki Akbar <akasa2444@gmail.com>
2023-07-07 16:33:59 +0700

    change lang pack attend

--- resources/lang/en/attend.php
+++ resources/lang/en/attend.php
@@ -6,5 +6,8 @@ return [
         'phone_number' => 'Phone Number',
         'email_helper' => 'Email used to register',
         'phone_helper' => 'Phone number used to register',
+        'is_certificate' => 'With Certificate',
+        'upload_pay' => 'Upload Payment Proof',
+        'upload_pay_helper' => 'Upload payment proof for verification',
     ]
 ];
--- resources/lang/id/attend.php
+++ resources/lang/id/attend.php
@@ -6,5 +6,8 @@ return [
         'phone_number' => 'Nomor Telepon',
         'email_helper' => 'Email yang digunakan untuk mendaftar',
         'phone_helper' => 'Nomor telepon yang digunakan untuk mendaftar',
+        'is_certificate' => 'Dengan Sertifikat',
+        'upload_pay' => 'Unggah Bukti Pembayaran',
+        'upload_pay_helper' => 'Unggah bukti pembayaran untuk diverifikasi',
     ]
 ];
