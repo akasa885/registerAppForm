3e5294100a00f6ae8cf77e96368e9a9d5788b7b2
Afu Sidhi <afumoons@gmail.com>
2023-07-07 16:07:44 +0700

    Change some label on view pages.pendaftaran.not_found_link

--- resources/views/pages/pendaftaran/not_found_link.blade.php
+++ resources/views/pages/pendaftaran/not_found_link.blade.php
@@ -1,10 +1,12 @@
 <div class="h-auto py-5">
-    <h3 class="text-center font-semibold p-10 sm:mx-auto">Link Pendaftaran Tak Ditemukan !!!</h3>
+    <h3 class="text-center font-semibold p-10 sm:mx-auto">Link Pertanyaan Tak Ditemukan !!!</h3>
     <!--begin::paragraph "pastikan link yang anda masukkan benar"-->
     <p class="text-center font-italic">
-        <strong> Pastikan link yang anda masukkan benar </strong>, jika anda merasa link yang anda masukkan sudah benar, silahkan hubungi kami melalui <br/>
+        <strong> Pastikan link yang anda masukkan benar </strong>, jika anda merasa link yang anda masukkan sudah benar,
+        silahkan hubungi kami melalui <br />
     </p>
     <div class="flex justify-center py-5">
-        <a href="https://wa.me/6285334727663" target="_blank" class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded-full">WhatsApp</a>
+        <a href="https://wa.me/6285334727663" target="_blank"
+            class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded-full">WhatsApp</a>
     </div>
 </div>
