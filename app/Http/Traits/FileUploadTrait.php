dfc15f5f23efc3652b9dad2e7df38fa12d48a294
Rizki Akbar <akasa2444@gmail.com>
2023-07-07 17:43:03 +0700

    change fileUploadTrait saveInvoice

--- app/Http/Traits/FileUploadTrait.php
+++ app/Http/Traits/FileUploadTrait.php
@@ -3,18 +3,21 @@ namespace App\Http\Traits;
 
 trait FileUploadTrait {
 
-    public function saveInvoice($file)
+    public function saveInvoice($file, $innerPath = null)
     {
         try {
             $fileNameWithExt = $file->getClientOriginalName();
             $filename = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
             $extension = $file->getClientOriginalExtension();
             $filenameSimpan = $filename.'__'.time().'.'.$extension;
+            if ($innerPath) {
+                $path = $file->storeAs('public/bukti_image/'.$innerPath, $filenameSimpan);
+                return $filenameSimpan;
+            }
             $path = $file->storeAs('public/bukti_image', $filenameSimpan);
             return $filenameSimpan;
         } catch (\Throwable $th) {
-            //throw $th;
-            return false;
+            throw $th;
         }
     }
 }
