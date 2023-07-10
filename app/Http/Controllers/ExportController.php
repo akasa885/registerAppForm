facfa4e272fc81a212152984130f847fbdc8eb36
Rizki Akbar <akasa2444@gmail.com>
2023-07-10 12:51:47 +0700

    create attendanceExport function and process at ExportController

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -7,6 +7,8 @@ use Maatwebsite\Excel\Facades\Excel;
 use App\Models\Link;
 
 use App\Exports\memberOfEvent;
+use App\Exports\attendanceOfEvent;
+use App\Models\Attendance;
 
 class ExportController extends Controller
 {
@@ -20,6 +22,22 @@ class ExportController extends Controller
             return Excel::download($member_export, $filename);
         } catch (\Throwable $th) {
             if(config('app.debug')) throw $th;
+
+            return abort(404, 'Exportable model not found');
+        }
+    }
+
+    public function attendanceExport(Attendance $attendance)
+    {
+        try {
+            $attendance_export = new attendanceOfEvent($attendance);
+            $attendance_export->exportProcess();
+
+            $filename = $attendance_export->getFilename();
+            return Excel::download($attendance_export, $filename);
+        } catch (\Throwable $th) {
+            if (config('app.debug')) throw $th;
+
             return abort(404, 'Exportable model not found');
         }
     }
