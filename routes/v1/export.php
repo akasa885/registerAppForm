ec851a825fa2c2e6f8af1e25b99c5a0e68b2e185
Rizki Akbar <akasa2444@gmail.com>
2023-07-10 12:51:04 +0700

    add new exportRoute attendance

--- routes/v1/export.php
+++ routes/v1/export.php
@@ -3,3 +3,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExportController;
 
 Route::get('/member-export/{link}', [ExportController::class, 'memberExport'])->name('member-export');
+Route::get('/attendance-export/{attendance}', [ExportController::class, 'attendanceExport'])->name('attendance-export');
