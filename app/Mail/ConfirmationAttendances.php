673a9de2a4596beec83bb1220f63d7f77ec085c3
Rizki Akbar <akasa2444@gmail.com>
2023-07-12 16:08:17 +0700

    create confirmation email attendance

---
+++ app/Mail/ConfirmationAttendances.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ConfirmationAttendances extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $data;
+    public $from_mail;
+    public $subject;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data, $from, $subject = null)
+    {
+        $this->data = $data;
+        $this->from_mail = $from;
+        $subject ? $this->subject = $subject : $this->setSubject();
+    }
+
+    public function setSubject()
+    {
+        $this->subject = __('mail.subject.attendance_confirmation');
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+        ->from($this->from_mail, "Event Organizer Upquality")
+        ->subject($this->subject)
+        ->view('mails.confirmation_attendance')
+        ->with('data', $this->data);;
+    }
+}
