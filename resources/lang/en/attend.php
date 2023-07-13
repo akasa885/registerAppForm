9391c96f2d0c36fe731d869a76e9e98ae19ccc57
Rizki Akbar <akasa2444@gmail.com>
2023-07-13 15:18:19 +0700

    change lang pack at attend

--- resources/lang/en/attend.php
+++ resources/lang/en/attend.php
@@ -2,11 +2,13 @@
 
 return [
     'form' => [
+        'full_name' => 'Full Name',
+        'full_name_helper' => 'Fill in your full name along with your title (if any). (leave blank if your full name already correct)',
         'email' => 'Email Address',
         'phone_number' => 'Phone Number',
         'email_helper' => 'Email used to register',
         'phone_helper' => 'Phone number used to register',
-        'is_certificate' => 'With Certificate (SKP IDI)',
+        'is_certificate' => 'With Certificate',
         'upload_pay' => 'Upload Payment Proof',
         'upload_pay_helper' => 'Upload payment proof for verification',
     ],
--- resources/lang/id/attend.php
+++ resources/lang/id/attend.php
@@ -2,11 +2,13 @@
 
 return [
     'form' => [
+        'full_name' => 'Nama Lengkap',
+        'full_name_helper' => 'Isikan Nama Lengkap Anda Berserta Gelarnya (Jika Ada). (kosongkan apabila nama lengkap sudah benar)',
         'email' => 'Alamat Email',
         'phone_number' => 'Nomor Telepon',
         'email_helper' => 'Email yang digunakan untuk mendaftar',
         'phone_helper' => 'Nomor telepon yang digunakan untuk mendaftar',
-        'is_certificate' => 'Dengan Sertifikat (SKP IDI)',
+        'is_certificate' => 'Dengan Sertifikat',
         'upload_pay' => 'Unggah Bukti Pembayaran',
         'upload_pay_helper' => 'Unggah bukti pembayaran untuk diverifikasi',
     ],
