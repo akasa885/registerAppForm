9ff6d51022f40456aa2d5f5bad4608e7491661a5
Rizki Akbar <akasa2444@gmail.com>
2023-07-17 10:08:23 +0700

    create new lang pack id & form_regist

---
+++ resources/lang/en/form_regist.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "full_name" => [
+        "help" => "Please fill in with your degree! (If any!)",
+    ],
+    "email" => [
+        "help" => "Please fill in with your valid email!",
+    ],
+];
---
+++ resources/lang/id/form_regist.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "full_name" => [
+        "help" => "Silahkan isi dengan gelar Anda! (Jika ada!)",
+    ],
+    "email" => [
+        "help" => "Silahkan isi dengan email Anda yang valid!",
+    ],
+];
