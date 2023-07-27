e21252de79b5503739b948c33d895c1ca4a94ead
Rizki Akbar <akasa2444@gmail.com>
2023-07-27 11:42:50 +0700

    create mail ReminderEvent

---
+++ app/Mail/ReminderEvent.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ReminderEvent extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $data;
+    public $from_mail;
+    public $subject;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data, $from, $subject = null)
+    {
+        $this->data = $data;
+        $this->from_mail = $from;
+        $subject ? $this->subject = $subject : $this->setSubject();
+    }
+
+    public function setSubject() {
+        $this->subject = __('mail.subject.reminder');
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+        ->from($this->from_mail, "Event Organizer Upquality")
+        ->subject($this->subject)
+        ->view('mails.reminder_event')
+        ->with('data', $this->data);
+    }
+}
