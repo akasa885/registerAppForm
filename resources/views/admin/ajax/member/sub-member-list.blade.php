4caaa1e644889a952a017327a4657bd10a701211
Rizki Akbar <akasa2444@gmail.com>
2023-08-01 13:35:00 +0700

    change view ajax at admin sub-member-list

--- resources/views/admin/ajax/member/sub-member-list.blade.php
+++ resources/views/admin/ajax/member/sub-member-list.blade.php
@@ -1,13 +1,22 @@
 <!--make list of sub members-->
 <div class="list-group">
-    @foreach ($particapants as $member)
-        <a href="#" class="list-group-item list-group-item-action" aria-current="true">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">{{ $member->full_name }}</h5>
-                <small>Peserta {{ $loop->iteration }}</small>
-            </div>
-            <p class="mb-1">Phone: {{ $member->contact_number }}</p>
-            <small>{{ $member->corporation }}</small>
-        </a>
-    @endforeach
+    @if ($particapants->isEmpty())
+        <!--if no sub members : text alert-->
+        <div class="alert alert-warning" role="alert">
+            <h4 class="alert-heading">Tidak ada peserta</h4>
+            <p>Belum ada peserta yang terdaftar.</p>
+            <hr>
+        </div>
+    @else
+        @foreach ($particapants as $member)
+            <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">{{ $member->full_name }}</h5>
+                    <small>Peserta {{ $loop->iteration }}</small>
+                </div>
+                <p class="mb-1">Phone: {{ $member->contact_number }}</p>
+                <small>{{ $member->corporation }}</small>
+            </a>
+        @endforeach
+    @endif
 </div>
