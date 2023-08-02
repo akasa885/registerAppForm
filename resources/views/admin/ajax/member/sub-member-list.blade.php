8c8a5edc54bb525c05ec403c9484c11266358f12
Rizki Akbar <akasa2444@gmail.com>
2023-08-02 09:40:48 +0700

    change view at ajax/member/sub-member-list

--- resources/views/admin/ajax/member/sub-member-list.blade.php
+++ resources/views/admin/ajax/member/sub-member-list.blade.php
@@ -12,7 +12,7 @@
             <a href="#" class="list-group-item list-group-item-action" aria-current="true">
                 <div class="d-flex w-100 justify-content-between">
                     <h5 class="mb-1">{{ $member->full_name }}</h5>
-                    <small>Peserta {{ $loop->iteration }}</small>
+                    <small># Peserta {{ $loop->iteration }}</small>
                 </div>
                 <p class="mb-1">Phone: {{ $member->contact_number }}</p>
                 <small>{{ $member->corporation }}</small>
