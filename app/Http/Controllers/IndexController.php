fb67f622b97c4d7f4ea72c31cb918e880a792140
Rizki Akbar <akasa2444@gmail.com>
2023-11-21 15:52:37 +0700

    change: index for purpose load more lazy

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,9 +8,15 @@ use App\Models\Link;
 
 class IndexController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $link = Link::orderBy('created_at', 'DESC')->get();
+        $link = Link::orderBy('id', 'DESC')->paginate(12);
+        if ($request->ajax()) {
+            $view = view('pages.home.data.list-link', ['link' => $link])->render();
+
+            return response()->json(['html' => $view]);
+        }
+
         return view('pages.home.home', ['link' => $link]);
     }   
 }
