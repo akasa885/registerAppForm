591162f077a0adf5f01ec26572dfda9913bc002f
Rizki Akbar <akasa2444@gmail.com>
2023-11-21 14:27:32 +0700

    fix: front footer

--- resources/views/includes/front_footer.blade.php
+++ resources/views/includes/front_footer.blade.php
@@ -1,11 +1,11 @@
 <!--begin::minimalist footer-->
 <footer class="bg-gray-800 py-4 px-6">
-    <div class="container mx-auto flex justify-between items-center">
-        <div class="text-sm text-gray-500">
+    <div class="grid grid-cols-1 container mx-auto md:flex justify-between items-center text-center gap-2">
+        <div class="text-sm text-gray-500 col-1">
             <span class="text-white"> &copy; {{ date('Y') }} </span> PT Utama Padma Qualiti. All rights reserved.
         </div>
 
-        <div class="text-sm text-gray-500">
+        <div class="text-sm text-gray-500 col-1">
             <a href="https://upquality.net" class="text-blue-500 hover:text-blue-600">{{ __('PT Utama Padma Qualiti') }}</a>
         </div>
     </div>
