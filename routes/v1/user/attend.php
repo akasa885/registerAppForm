822ad960837e20de3131718668d7ee93a6ace31b
Rizki Akbar <akasa2444@gmail.com>
2023-11-28 14:47:40 +0700

    add: new route user attend

--- routes/v1/user/attend.php
+++ routes/v1/user/attend.php
@@ -3,3 +3,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AttendanceController;
 Route::get('/{link}', [AttendanceController::class, 'page'])->name('link');
 Route::post('/{attendance}', [AttendanceController::class, 'attending']);
+Route::get('/attending/payment/waiting/{attendance}/{orderNumber}', [AttendanceController::class, 'waitingPayment'])->name('waiting-payment');
