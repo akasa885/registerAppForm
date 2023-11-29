692bff73def2e029a6cf22775e2c8b235bd04944
Rizki Akbar <akasa2444@gmail.com>
2023-11-29 11:07:53 +0700

    change: update show at admin/SiteSettingController

--- app/Http/Controllers/Admin/SiteSettingController.php
+++ app/Http/Controllers/Admin/SiteSettingController.php
@@ -3,19 +3,51 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Validation\Validator;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class SiteSettingController extends Controller
 {
+    private function generateMenu()
+    {
+        return [
+            [
+                'label' => 'Website',
+                'route' => 'admin.setting.view',
+                'link' => route('admin.setting.view')
+            ],
+            [
+                'label' => 'Midtrans',
+                'route' => 'admin.setting.midtrans',
+                'link' => route('admin.setting.midtrans')
+            ]
+        ];
+    }
+
     public function site()
     {
-        //
+        $menu = $this->generateMenu();
+
+        $content = view('admin.pages.setting.site')->render();
+
+        return view('admin.pages.setting.view', [
+            'title' => 'Pengaturan Website',
+            'header' => 'Pengaturan Website',
+            'subheader' => 'This is a page where you can manage your regular website settings',
+        ], compact('menu', 'content'));
     }
 
     public function midtrans()
     {
-        //
+        $menu = $this->generateMenu();
+
+        $content = view('admin.pages.setting.midtrans')->render();
+
+        return view('admin.pages.setting.view', [
+            'title' => 'Pengaturan Midtrans',
+            'header' => 'Pengaturan Midtrans',
+            'subheader' => 'This is a page where you can manage your midtrans settings',
+        ], compact('menu', 'content'));
     }
 
     public function midtransUpdate(Request $request)
@@ -24,7 +56,7 @@ class SiteSettingController extends Controller
             'midtrans_client_key' => 'required',
             'midtrans_server_key' => 'required',
             'midtrans_merchant_id' => 'required',
-            'midtrans_environment' => 'required, in:production,sandbox',
+            'midtrans_environment' => 'required|in:production,sandbox',
         ], [
             'midtrans_client_key.required' => 'Client Key is required',
             'midtrans_server_key.required' => 'Server Key is required',
@@ -41,5 +73,7 @@ class SiteSettingController extends Controller
         if ($validator->fails()) {
             return redirect()->back()->withErrors($validator)->withInput();
         }
+
+        $validated = $validator->validated();
     }
 }
