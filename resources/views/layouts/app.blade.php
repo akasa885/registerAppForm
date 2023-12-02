3f6617216f546a900a803fc5da6184644b7f3bf9
Rizki Akbar <akasa2444@gmail.com>
2023-12-02 11:32:16 +0700

    fix: front nav position

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,12 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Eform for Registration Management">
-    <meta name="keywords" content="eform, registration, management, laravel, php, mysql, bootstrap, tailwindcss, alpinejs, livewire, rizkiakbar, rizki, akbar">
+    <meta name="keywords"
+        content="eform, registration, management, laravel, php, mysql, bootstrap, tailwindcss, alpinejs, livewire, rizkiakbar, rizki, akbar">
     <meta name="author" content="Rizki Akbar">
     <meta name="author_email" content="rixak98@gmail.com">
     <meta name="application-name" content="Eform for Registration Management">
@@ -20,7 +22,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    
+
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('/vendor/jquery-3.4.min.js') }}"></script>
@@ -30,48 +32,52 @@
             content: "*";
             color: red;
         }
+
         .dropdown:hover .dropdown-menu {
             display: block;
         }
     </style>
 </head>
+
 <body class="bg-gray-100 h-screen antialiased leading-none font-sans">
     <div id="app">
         <header class="bg-blue-900 py-5">
-            <div class="container mx-auto flex justify-between items-center px-6">
-                <div>
+            <div class="container mx-auto flex flex-wrap justify-between gap-2 items-center px-6">
+                <!--begin::logo-->
+                <div class="block">
                     <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
                         {{ config('app.name', 'Laravel') }}
                     </a>
                 </div>
-                <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
+                <!--end::logo-->
+                <nav class="md:space-x-4 flex items-center text-left flex-wrap gap-2 text-gray-300 text-sm sm:text-base">
+                    <!--begin::lang-->
                     @include('partials.language_switcher_front')
+                    <!--end::lang-->
                     @guest
-                        {{-- <a class="no-underline hover:underline" href="{{ route('admin.login') }}">{{ __('Login') }}</a> --}}
-                        @if (Route::has('register'))
-                            <a class="no-underline hover:underline" href="{{ route('admin.register') }}">{{ __('Register') }}</a>
-                        @endif
+                        <!-- Authentication Links -->
                     @else
+                    <div class="space-x-1">
                         <a href="{{ route('admin.dashboard') }}">
                             <span>{{ Auth::user()->name }}</span>
                         </a>
 
-                        <a href="{{ route('admin.logout') }}"
-                           class="no-underline hover:underline"
-                           onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-                        <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" class="hidden">
-                            {{ csrf_field() }}
-                        </form>
+                        <a href="{{ route('admin.logout') }}" class="no-underline hover:underline" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                    </div>
                     @endguest
                 </nav>
             </div>
         </header>
-
+        <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" class="hidden">
+            {{ csrf_field() }}
+        </form>
         @yield('content')
 
         @include('includes.front_footer')
     </div>
     @stack('scripts')
 </body>
+
 </html>
