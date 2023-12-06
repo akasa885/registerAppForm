590eec0945a99c330111269be64f95e83e8f66fa
Rizki Akbar <akasa2444@gmail.com>
2023-12-06 15:03:41 +0700

    create: component admin delete-js-alerted

---
+++ resources/views/components/admin/delete-js-alerted.blade.php
@@ -0,0 +1,46 @@
+@push('scripts')
+<script>
+    let deleteScriptJs = (url, message = "Anda yakin ingin menghapus data ini ?") => {
+        Swal.fire({
+            title: 'Apakah anda yakin?',
+            text: message,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Ya, hapus!',
+            cancelButtonText: 'Tidak, batalkan!',
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: url,
+                    type: 'DELETE',
+                    dataType: 'json',
+                    success: function (data) {
+                        Swal.fire({
+                            title: 'Berhasil!',
+                            text: data.message,
+                            icon: 'success',
+                            confirmButtonColor: '#3085d6',
+                            confirmButtonText: 'OK'
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                location.reload();
+                            }
+                        })
+                    },
+                    error: function (data) {
+                        Swal.fire({
+                            title: 'Gagal!',
+                            text: data.message,
+                            icon: 'error',
+                            confirmButtonColor: '#3085d6',
+                            confirmButtonText: 'OK'
+                        })
+                    }
+                });
+            }
+        });
+    }
+</script>
+@endpush
