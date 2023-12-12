a8d7478fa8b806d16dd9a2d9cf4de22e83b218f8
Rizki Akbar <akasa2444@gmail.com>
2023-12-12 10:02:52 +0700

    create: trait SiteContainTrait

---
+++ app/Http/Traits/SiteContainTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Traits;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+
+trait SiteContainTrait {
+
+    public function getInformationFile()
+    {
+        try {
+            $information = Storage::disk('local')->get('key/site.json');
+            $information = json_decode($information, true);
+
+            return $information;
+        } catch (FileNotFoundException $e) {
+            $information = [
+                'sitename' => config('app.name'),
+                'copyright' => ' PT Utama Padma Qualiti',
+                'developedby' => 'IT Department',
+                'description' => 'Website E-Form Internal Organisasi',
+                'keywords' => 'eform, e-form, internal form, form registrasi, form internal, form',
+            ];
+
+            return $information;
+        }
+    }
+
+    public function getDataGeneralWebsite()
+    {
+        $sitename = config('app.name');
+        $siteCopyRight = ' PT Utama Padma Qualiti';
+        $developedBy = 'IT Department';
+
+        return [$sitename, $siteCopyRight, $developedBy];
+    }
+}
