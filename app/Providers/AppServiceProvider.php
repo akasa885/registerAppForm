83765a2e4c4571c1625bde87b75257373fdb9b22
Rizki Akbar <akasa2444@gmail.com>
2023-12-12 10:30:36 +0700

    feat(global variable) : add new global variable from boot AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,10 +5,13 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Helpers\UrlAccessLocalCheck;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use App\Http\Traits\SiteContainTrait;
 use App\Helpers\Midtrans;
 
 class AppServiceProvider extends ServiceProvider
 {
+    use SiteContainTrait;
+
     /**
      * Register any application services.
      *
@@ -32,6 +35,11 @@ class AppServiceProvider extends ServiceProvider
             'MorphLinks' => 'App\Models\Link',
             'MorphAttend' => 'App\Models\Attendance',
         ]);
+
+        view()->share('information_site', $this->getInformationFile());
+        $this->app->singleton('information_site', function () {
+            return $this->getInformationFile();
+        });
 
         view()->composer('partials.language_switcher_front', function ($view) {
             $view->with('current_locale', app()->getLocale());
