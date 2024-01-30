723fb8e0c0dff1d8d56517c61461204ae863b59a
Rizki Akbar <akasa2444@gmail.com>
2024-01-30 13:22:25 +0700

    add: new lang pack transaction

--- resources/lang/en/transaction.php
+++ resources/lang/en/transaction.php
@@ -3,4 +3,6 @@
 return [
     'success-payment-auto-confirmed' => 'Payment success, your order has been confirmed automatically',
     'success-payment-title' => 'Payment Success :order_number',
+    'pending-payment-title' => 'Payment Pending :order_number',
+    'pending-payment-message' => 'Your payment is still pending, please complete your payment',
 ];
--- resources/lang/id/transaction.php
+++ resources/lang/id/transaction.php
@@ -3,4 +3,6 @@
 return [
     'success-payment-auto-confirmed' => 'Pembayaran berhasil, pesanan Anda telah dikonfirmasi secara otomatis',
     'success-payment-title' => 'Pembayaran Berhasil :order_number',
+    'pending-payment-title' => 'Pembayaran Pending :order_number',
+    'pending-payment-message' => 'Pembayaran Anda masih tertunda, silahkan selesaikan pembayaran Anda',
 ];
