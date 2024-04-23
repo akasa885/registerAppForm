1dcf4ede88c2d481712e8f6db3fc13247841a7ff
Rizki Akbar <akasa2444@gmail.com>
2024-04-23 11:58:17 +0700

    change: upPay information email

--- resources/views/pages/pendaftaran/upPay.blade.php
+++ resources/views/pages/pendaftaran/upPay.blade.php
@@ -11,9 +11,11 @@
                     <x-front-message-success-alert :message="$message" />
                 @elseif($used)
                     @if (config('app.locale') == 'id')
-                        <h3 class="font-semibold p-10 sm:mx-auto">Anda telah melakukan pembayaran, silahkan tunggu balasan pada email anda !</h3>
+                        <h3 class="font-semibold p-10 sm:mx-auto">Anda telah melakukan pembayaran, silahkan tunggu balasan
+                            pada email anda !</h3>
                     @else
-                        <h3 class="font-semibold p-10 sm:mx-auto">You have made a payment, please wait for a reply to your email !</h3>
+                        <h3 class="font-semibold p-10 sm:mx-auto">You have made a payment, please wait for a reply to your
+                            email !</h3>
                     @endif
                 @elseif($not_found)
                     @include('pages.pendaftaran.pay_confirmation.not_found_token')
@@ -25,22 +27,25 @@
                     <form class="w-full px-6 mb-5 space-y-6 sm:px-10 sm:space-y-8" method="POST"
                         action="{{ route('form.pay.store', ['payment' => $pay_code]) }}" enctype="multipart/form-data">
                         @csrf
-                        @if($message = Session::get('info'))
-                        <!-- begin::alert Info -->
-                        <div class="alert alert-info bg-blue-100 rounded-lg py-5 px-6  w-full alert-dismissible fade show" role="alert">
-                            <svg width="24" height="24" class="alert-icon w-10 h-10"
-                                viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="10"
-                                    fill="currentColor" />
-                                <rect x="11" y="14" width="7" height="2" rx="1"
-                                    transform="rotate(-90 11 14)" fill="currentColor" />
-                                <rect x="11" y="17" width="2" height="2" rx="1"
-                                    transform="rotate(-90 11 17)" fill="currentColor" />
-                            </svg>
-                            <span class="ml-4 alert-message">{{ $message }}</span>
-                            <button type="button" class="btn-close box-content w-4 h-4 p-1 ml-auto text-yellow-900 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-yellow-900 hover:opacity-75 hover:no-underline" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                        <!-- end::alert Info -->
+                        @if ($message = Session::get('info'))
+                            <!-- begin::alert Info -->
+                            <div class="alert alert-info bg-blue-100 rounded-lg py-5 px-6  w-full alert-dismissible fade show"
+                                role="alert">
+                                <svg width="24" height="24" class="alert-icon w-10 h-10" viewBox="0 0 24 24"
+                                    fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="10"
+                                        fill="currentColor" />
+                                    <rect x="11" y="14" width="7" height="2" rx="1"
+                                        transform="rotate(-90 11 14)" fill="currentColor" />
+                                    <rect x="11" y="17" width="2" height="2" rx="1"
+                                        transform="rotate(-90 11 17)" fill="currentColor" />
+                                </svg>
+                                <span class="ml-4 alert-message">{{ $message }}</span>
+                                <button type="button"
+                                    class="btn-close box-content w-4 h-4 p-1 ml-auto text-yellow-900 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-yellow-900 hover:opacity-75 hover:no-underline"
+                                    data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                            <!-- end::alert Info -->
                         @endif
                         <!--begin::top rigth timer-->
                         <div class="flex justify-end">
@@ -51,38 +56,41 @@
                         </div>
                         <!--end::top rigth timer-->
                         @if (isset($snap_token))
-                        @if (config('app.locale') == 'id')
-                            <p class="sm:mt-4">Kepada Bpk/Ibu, <strong> {{ $member->full_name }} </strong> </p>
-                            <p>
-                                Terima kasih telah melakukan pendaftaran {{ $link->title }}.<br> <strong> Silahkan
-                                    melakukan pembayaran dengan klik tombol dibawah ini </strong>
-                            </p>
-                        @else
-                            <p class="sm:mt-4">To Mr/Mrs, <strong> {{ $member->full_name }} </strong> </p>
-                            <p>
-                                Thank you for registering {{ $link->title }}.<br> <strong> Please make a payment by
-                                    clicking the button below </strong>
-                            </p>
-                        @endif
-                        <div class="flex flex-wrap">
-                            <!-- begin::Action-->
-                            <a target="__blank" href="{{ $snap_redirect }}" id="button" class="w-full select-none font-bold whitespace-no-wrap p-3 rounded-lg text-base leading-normal no-underline text-gray-100 bg-blue-500 hover:bg-blue-700 sm:py-4">Continue To Payment</a>
-                            <!-- end::Action-->
-                        </div>
+                            @if (config('app.locale') == 'id')
+                                <p class="sm:mt-4">Kepada Bpk/Ibu, <strong> {{ $member->full_name }} </strong> </p>
+                                <p>
+                                    Terima kasih telah melakukan pendaftaran {{ $link->title }}.<br> <strong> Silahkan
+                                        melakukan pembayaran dengan klik tombol dibawah ini </strong>
+                                </p>
+                            @else
+                                <p class="sm:mt-4">To Mr/Mrs, <strong> {{ $member->full_name }} </strong> </p>
+                                <p>
+                                    Thank you for registering {{ $link->title }}.<br> <strong> Please make a payment by
+                                        clicking the button below </strong>
+                                </p>
+                            @endif
+                            <div class="flex flex-wrap">
+                                <!-- begin::Action-->
+                                <a target="__blank" href="{{ $snap_redirect }}" id="button"
+                                    class="w-full select-none font-bold whitespace-no-wrap p-3 rounded-lg text-base leading-normal no-underline text-gray-100 bg-blue-500 hover:bg-blue-700 sm:py-4">Continue
+                                    To Payment</a>
+                                <!-- end::Action-->
+                            </div>
                         @else
                             @if (config('app.locale') == 'id')
                                 <p class="sm:mt-4">Kepada Bpk/Ibu, <strong> {{ $member->full_name }} </strong> </p>
                                 <p>
                                     Terima kasih telah melakukan pendaftaran {{ $link->title }}.<br> <strong> Informasi
-                                        terkait pembayaran telah kami kirimkan ke email anda </strong>, <br>Silahkan upload
+                                        terkait pembayaran telah kami kirimkan ke email anda. Silahkan buka pada Inbox /
+                                        Spam </strong>, <br>Silahkan upload
                                     bukti bayar anda pada form dibawah ini.
                                 </p>
                             @else
                                 <p class="sm:mt-4">To Mr/Mrs, <strong> {{ $member->full_name }} </strong> </p>
                                 <p>
-                                    Thank you for registering {{ $link->title }}.<br> <strong> Payment information has been
-                                        sent to your email </strong>, <br>Please upload your proof of payment in the form
-                                    below.
+                                    Thank you for registering {{ $link->title }}.<br> <strong> Payment information has
+                                        been sent to your email. Please open it in Inbox / Spam </strong>, <br>Please upload
+                                    your proof of payment in the form below.
                                 </p>
                             @endif
                             <div class="flex">
@@ -116,29 +124,48 @@
 
 @push('scripts')
     <script>
-        $('document').ready(function () {
+        $('document').ready(function() {
             @if ($timeLeft)
-            var timerCounter = () => {
-                let time_left = "{{ $timeLeft }}";
-                let time_left_payment = document.getElementById('time_left_payment');
-                let time_left_array = time_left.split(':');
-                let hours = time_left_array[0];
-                let minutes = time_left_array[1];
-                let seconds = time_left_array[2];
-                let x = setInterval(function () {
-                    if (seconds > 0) { seconds--; } else { seconds = 59;
-                        if (minutes > 0) { minutes--; } else { minutes = 59;
-                            if (hours > 0) { hours--; } else { hours = 0; minutes = 0; seconds = 0; clearInterval(x); window.location.reload(); }
+                var timerCounter = () => {
+                    let time_left = "{{ $timeLeft }}";
+                    let time_left_payment = document.getElementById('time_left_payment');
+                    let time_left_array = time_left.split(':');
+                    let hours = time_left_array[0];
+                    let minutes = time_left_array[1];
+                    let seconds = time_left_array[2];
+                    let x = setInterval(function() {
+                        if (seconds > 0) {
+                            seconds--;
+                        } else {
+                            seconds = 59;
+                            if (minutes > 0) {
+                                minutes--;
+                            } else {
+                                minutes = 59;
+                                if (hours > 0) {
+                                    hours--;
+                                } else {
+                                    hours = 0;
+                                    minutes = 0;
+                                    seconds = 0;
+                                    clearInterval(x);
+                                    window.location.reload();
+                                }
+                            }
                         }
-                    }
-                    // if seconds < 10 then add 0 before seconds
-                    if (seconds < 10) { seconds = '0' + seconds; }
-                    time_left_payment.innerHTML = hours + ":" + minutes + ":" + seconds;
-                    if (hours == 0 && minutes == 0 && seconds == 0) { clearInterval(x); window.location.reload(); }
-                }, 1000);
-            }
+                        // if seconds < 10 then add 0 before seconds
+                        if (seconds < 10) {
+                            seconds = '0' + seconds;
+                        }
+                        time_left_payment.innerHTML = hours + ":" + minutes + ":" + seconds;
+                        if (hours == 0 && minutes == 0 && seconds == 0) {
+                            clearInterval(x);
+                            window.location.reload();
+                        }
+                    }, 1000);
+                }
 
-            timerCounter();
+                timerCounter();
             @endif
         })
     </script>
