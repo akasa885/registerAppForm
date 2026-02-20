f9502d93afe760e85ee27cd24ecc4e328d97a8ba
Rizki Akbar <akasa2444@gmail.com>
2026-02-20 10:35:17 +0700

    fix: change update user manage role & password logic

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -31,14 +31,13 @@ class UserController extends Controller
     }
 
     public function index()
-    {        
-        if(Gate::allows('isSuperAdmin')){
+    {
+        if (Gate::allows('isSuperAdmin')) {
             $users = User::all();
             return view('admin.pages.users.view', ['users' => $users, 'title' => 'Pengaturan User']);
-        }else{
+        } else {
             return redirect()->route('admin.dashboard');
-        }        
-
+        }
     }
 
     public function create()
@@ -54,9 +53,9 @@ class UserController extends Controller
             'name' => ['required', 'alpha'],
             'email' => ['required', 'email', 'unique:users,email'],
             'role' => ['required'],
-            'password' => ['required', 'confirmed', Password::min(6)],            
+            'password' => ['required', 'confirmed', Password::min(6)],
         ]);
-        
+
         try {
             $user  = new User;
             $user->password = Hash::make($request->password);
@@ -71,20 +70,21 @@ class UserController extends Controller
             report($th);
 
             return redirect()->back()->withInput()->with('error', 'Something went wrong !');
-        }        
+        }
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $this->authorize('isSuperAdmin');
         $role = User::TYPE_ROLE;
         $user = User::find($id);
-        if($user != null){
+        if ($user != null) {
             return view('admin.pages.users.edit', ['user' => $user, 'role' => $role]);
             // return response()->json(['success' => true, 'message' => "Found!", 'user' => $user], 200);
-        }else{
+        } else {
             // return response()->json(['success' => false, 'message' => "not found", 'user' => ''], 404);
             abort(404);
-        }        
+        }
     }
 
     public function edit()
@@ -157,20 +157,21 @@ class UserController extends Controller
         $this->validate($request, [
             'name' => ['required', 'alpha'],
             'email' => ['required', 'email'],
-            'role' => ['required'],                        
+            'role' => ['required'],
+            'new_password' => ['nullable', Password::min(8)],
         ]);
 
         try {
             $user  = User::findorfail($id);
-            ($request->new_password!= null 
-                ? $user->password = Hash::make($request->password) 
+            ($request->new_password != null
+                ? $user->password = Hash::make($request->new_password)
                 : '');
             $user->email = $request->email;
-            $user->name = ucwords($request->name);            
+            $user->name = ucwords($request->name);
             $user->role = $request->role;
             $user->save();
 
-            return redirect()->route('admin.users.view');
+            return redirect()->route('admin.users.view')->with('success', 'User : ' . $user->name . ' updated successfully');
         } catch (\Throwable $th) {
             abort(500);
             return redirect()->back()->withInput()->with('error', 'Something went wrong !');
@@ -180,12 +181,11 @@ class UserController extends Controller
     public function deactive($id)
     {
         $user = User::find($id);
-        if($user != null){        
+        if ($user != null) {
             $user->status = 0;
             $user->save();
-            return response()->json(['success' => true, 'message' => 'deactived success !!']);    
-            
-        }else{
+            return response()->json(['success' => true, 'message' => 'deactived success !!']);
+        } else {
             return response()->json(['success' => false, 'message' => 'record not found']);
         }
         return response()->json(['success' => false, 'message' => "request gagal!"]);
@@ -194,12 +194,11 @@ class UserController extends Controller
     public function active($id)
     {
         $user = User::find($id);
-        if($user != null){        
+        if ($user != null) {
             $user->status = 1;
             $user->save();
-            return response()->json(['success' => true, 'message' => 'actived success !!']);    
-            
-        }else{
+            return response()->json(['success' => true, 'message' => 'actived success !!']);
+        } else {
             return response()->json(['success' => false, 'message' => 'record not found']);
         }
         return response()->json(['success' => false, 'message' => 'request gagal!']);
@@ -209,43 +208,43 @@ class UserController extends Controller
     {
         $me = Auth::user();
         $data = User::orderBy('created_at', 'DESC')->where('id', '!=', $me->id)->get();
-        $edit="";
-            return DataTables::of($data)
-        ->editColumn("status", function ($data) {
-            if($data->status){
-                return "<div class=\"mb-2 mr-2 badge badge-pill badge-success\">Aktif</div>";
-            }else{
-            return "<div class=\"mb-2 mr-2 badge badge-pill badge-danger\">Non Aktif</div>";
-            }         
-        })       
-        ->editColumn('role', function ($data){
-            return ucwords($data->role);
-        })
-        ->addColumn('Options', function ($data){
-            $edit = "<a href=".route('admin.users.edit',['id' => $data->id])." aria-expanded=\"false\" class=\"mb-2 mr-2 badge badge-info\" style=\"margin-right:0.2rem;\">
+        $edit = "";
+        return DataTables::of($data)
+            ->editColumn("status", function ($data) {
+                if ($data->status) {
+                    return "<div class=\"mb-2 mr-2 badge badge-pill badge-success\">Aktif</div>";
+                } else {
+                    return "<div class=\"mb-2 mr-2 badge badge-pill badge-danger\">Non Aktif</div>";
+                }
+            })
+            ->editColumn('role', function ($data) {
+                return ucwords($data->role);
+            })
+            ->addColumn('Options', function ($data) {
+                $edit = "<a href=" . route('admin.users.edit', ['id' => $data->id]) . " aria-expanded=\"false\" class=\"mb-2 mr-2 badge badge-info\" style=\"margin-right:0.2rem;\">
                                                 <span class=\"btn-icon-wrapper pr-2 opacity-7\">
                                                     <i class=\"pe-7s-magic-wand fa-w-20\"></i>
                                                 </span>
                                                 Edit
                                             </a>";
-            if($data->status){
-                $edit .= "<a href=\"javascript:deactivateAction(".$data->id.");\" aria-expanded=\"false\" class=\"mb-2 mr-2 badge badge-danger\" style=\"margin-right:0.2rem;\">
+                if ($data->status) {
+                    $edit .= "<a href=\"javascript:deactivateAction(" . $data->id . ");\" aria-expanded=\"false\" class=\"mb-2 mr-2 badge badge-danger\" style=\"margin-right:0.2rem;\">
                                                 <span class=\"btn-icon-wrapper pr-2 opacity-7\">
                                                     <i class=\"fa fa-trash fa-w-20\"></i>
                                                 </span>
                                                 Non Aktifkan
                                             </a>";
-            }else{
-                $edit .= "<a href=\"javascript:activateAction(".$data->id.");\" aria-expanded=\"false\" class=\"mb-2 mr-2 badge badge-success\" style=\"margin-right:0.2rem;\">
+                } else {
+                    $edit .= "<a href=\"javascript:activateAction(" . $data->id . ");\" aria-expanded=\"false\" class=\"mb-2 mr-2 badge badge-success\" style=\"margin-right:0.2rem;\">
                                                 <span class=\"btn-icon-wrapper pr-2 opacity-7\">
                                                     <i class=\"pe-7s-magic-wand fa-w-20\"></i>
                                                 </span>
                                                 Aktifkan
                                             </a>";
-            }
-            return $edit;
-        })
-        ->rawColumns(['role','Options', 'status'])
-        ->make(true);
+                }
+                return $edit;
+            })
+            ->rawColumns(['role', 'Options', 'status'])
+            ->make(true);
     }
 }
