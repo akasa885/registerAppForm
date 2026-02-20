e392f3f8e1dab7c0cf9f87c0bf0e4e9f0f0862a2
Rizki Akbar <akasa2444@gmail.com>
2026-02-20 11:06:00 +0700

    chore: update dependencies and improve Tailwind CSS configuration
    - Replaced @tailwindcss/ui with @tailwindcss/forms in package.json
    - Updated autoprefixer, axios, postcss, postcss-nested, and tailwindcss to their latest versions
    - Enhanced tailwind.config.js to include new color themes and updated content paths
    - Refactored webpack.mix.js for consistency in code style and improved readability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,38 @@
 module.exports = {
-  purge: [
-    './resources/views/**/*.blade.php',
-    './resources/css/**/*.css',
-  ],
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/ui'),
-  ]
-}
+    content: [
+        "./resources/views/**/*.blade.php",
+        "./resources/css/**/*.css",
+        "./resources/js/**/*.js",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                base: {
+                    50: "#f5f7ff",
+                    100: "#e0e7ff",
+                    200: "#c7d2fe",
+                    300: "#a5b4fc",
+                    400: "#818cf8",
+                    500: "#6366f1",
+                    600: "#4f46e5",
+                    700: "#4338ca",
+                    800: "#3730a3",
+                    900: "#312e81",
+                },
+                yellow: {
+                    50: "#fffbeb",
+                    100: "#fef3c7",
+                    200: "#fde68a",
+                    300: "#fcd34d",
+                    400: "#fbbf24",
+                    500: "#f59e0b",
+                    600: "#d97706",
+                    700: "#b45309",
+                    800: "#92400e",
+                    900: "#78350f",
+                },
+            },
+        },
+    },
+    plugins: [require("@tailwindcss/forms")],
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
-const mix = require('laravel-mix');
-const WebpackObfuscator = require('webpack-obfuscator');
+const mix = require("laravel-mix");
+const WebpackObfuscator = require("webpack-obfuscator");
 
 /*
 |--------------------------------------------------------------------------
@@ -10,32 +10,35 @@ const WebpackObfuscator = require('webpack-obfuscator');
 | for your Laravel application. By default, we are compiling the Sass
 | file for the application as well as bundling up all the JS files.
 |
- */
+*/
 
-mix
-  .js('resources/js/app.js', 'public/js')
-  .postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('postcss-nested'),
-    require('autoprefixer'),
-  ]);
-mix
-  .js('resources/js/pages/page.js', 'public/js')
-mix
-  .js('resources/js/front/front.js', 'public/js');
+mix.js("resources/js/app.js", "public/js").postCss(
+    "resources/css/app.css",
+    "public/css",
+    [
+        require("postcss-import"),
+        require("tailwindcss"),
+        require("postcss-nested"),
+        require("autoprefixer"),
+    ],
+);
+
+mix.js("resources/js/pages/page.js", "public/js");
+
+mix.js("resources/js/front/front.js", "public/js");
 
 if (mix.inProduction()) {
-  mix
-    .version();
-  mix
-    .webpackConfig({
-      plugins: [
-          new WebpackObfuscator({
-              rotateStringArray: true,
-              stringArray: true,
-              stringArrayThreshold: 0.75,
-          }, ['app.js'])
-      ]
-  });
+    mix.version();
+    mix.webpackConfig({
+        plugins: [
+            new WebpackObfuscator(
+                {
+                    rotateStringArray: true,
+                    stringArray: true,
+                    stringArrayThreshold: 0.75,
+                },
+                ["app.js"],
+            ),
+        ],
+    });
 }
